8874ab1fe55f841fe37fb576d289fd4814cf4150
Valerii Pometun <vmpometun@gmail.com>
2022-12-27 16:28:40 +0200

    modals

---
+++ js/locations.js
@@ -0,0 +1,15 @@
+(() => {
+   const refs = {
+     openModalBtn: document.querySelector("[data-locations-open]"),
+     closeModalBtn: document.querySelector("[data-locations-close]"),
+     modal: document.querySelector("[data-locations]"),
+   };
+ 
+   refs.openModalBtn.addEventListener("click", toggleModal);
+   refs.closeModalBtn.addEventListener("click", toggleModal);
+ 
+   function toggleModal() {
+      document.body.classList.toggle("no-scroll");
+      refs.modal.classList.toggle("is-hidden");
+   }
+ })();
