c2f292bd8c99e95bb56ccb56812d12198a22bf87
Valerii Pometun <vmpometun@gmail.com>
2022-12-28 09:29:40 +0200

    done with project

---
+++ js/_slider.js
@@ -0,0 +1,10 @@
+$(document).ready(function() { 
+    $('.slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 1650,
+        fade: true,
+        cssEase: 'linear',
+        autoplay: true
+    });
+});
